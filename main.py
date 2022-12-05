2b973b62e8f907bddf15801e01620317d6137f21
erwinzulfikar <erwin_zulfikar@yahoo.com>
2022-12-05 11:31:54 +0700

    Finish real data from BMKG.go.id

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
 """
 Aplikasi deteksi gempa terkini
 MODULARISASI DENGAN FUNCTION
+MODULARISASI DENGAN PACKAGE
 """
-
-def ekstraksi_data():
-    """
-    Tanggal: 24 November 2022
-    Waktu: 11:51:32 WIB
-    Magnitudo: 3.8
-    Kedalaman: 10 km
-    Lokasi: LS=3.80 BT=128.50
-    Pusat gempa: Pusat gempa berada di laut 30 km barat daya Maluku Tengah
-    Dirasakan: Dirasakan (Skala MMI): I-II Ambon
-    :return:
-    """
-    hasil = dict()
-    hasil['tanggal'] = '24 November 2022'
-    hasil['waktu'] = '11:51:32 WIB'
-    hasil['magnitudo'] = 3.8
-    hasil['kedalaman'] = '10 km'
-    hasil['lokasi'] = {'ls': 3.80, 'bt': 128.50}
-    hasil['pusat'] = 'Pusat gempa berada di laut 30 km barat daya Maluku Tengah'
-    hasil['dirasakan'] = 'Dirasakan (Skala MMI): I-II Ambon'
-
-    return hasil
-
-
-def tampilkan_data(result):
-    print('Gempa Terakhir berdasarkan BMKG')
-    print(f"Tanggal {result['tanggal']}")
-    print(f"Waktu {result['waktu']}")
-    print(f"Magnitudo {result['magnitudo']}")
-    print(f"Kedalaman {result['kedalaman']}")
-    print(f"Lokasi LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
-    print(f"Pusat {result['pusat']}")
-    print(f"Dirasakan {result['dirasakan']}")
+from gempaterkini import ekstraksi_data, tampilkan_data
 
 if __name__ == '__main__':
     print('Aplikasi Utama')
     result = ekstraksi_data()
     tampilkan_data(result)
+
+"""
+*Format lain untuk import
+import gempaterkini
+
+if __name__ == '__main__':
+    print('Aplikasi Utama')
+    result = gempaterkini.ekstraksi_data()
+    gempaterkini.tampilkan_data(result)
+"""
