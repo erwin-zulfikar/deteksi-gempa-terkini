08e1eeabba8e6ba57d8c043324ed44e3e1af987f
erwinzulfikar <erwin_zulfikar@yahoo.com>
2022-12-09 09:09:30 +0700

    Finish real data from BMKG.go.id

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ Aplikasi deteksi gempa terkini
 MODULARISASI DENGAN FUNCTION
 MODULARISASI DENGAN PACKAGE
 """
-from gempaterkini import ekstraksi_data, tampilkan_data
 
-if __name__ == '__main__':
-    print('Aplikasi Utama')
-    result = ekstraksi_data()
-    tampilkan_data(result)
-
-"""
-*Format lain untuk import
 import gempaterkini
 
 if __name__ == '__main__':
     print('Aplikasi Utama')
     result = gempaterkini.ekstraksi_data()
     gempaterkini.tampilkan_data(result)
+
+"""
+*Format lain untuk import
+from gempaterkini import ekstraksi_data, tampilkan_data
+
+if __name__ == '__main__':
+    print('Aplikasi Utama')
+    result = ekstraksi_data()
+    tampilkan_data(result)
 """
